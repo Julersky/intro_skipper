b9ce4e38d6ae3f0644a5fafa63a734c18ec7bce5
Julersky <eric.eibl@hotmail.fr>
2023-10-27 20:37:53 +0200

    final

---
+++ content.js
@@ -0,0 +1,38 @@
+
+
+(() => {
+    window.addEventListener('load', () => {
+        bodyObserver.observe(document.body, observerOptions);
+    });
+})();
+
+const observerOptions = {
+    subtree: true,
+    childList: true
+};
+
+const bodyObserver = new MutationObserver(mutations => {
+    console.log('hello')
+    mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+                if(node instanceof HTMLElement){
+                    const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
+                    if(skipButton){
+                        console.log('Skip button seen, clicking..');
+                        // small delay just for visuals
+                        setTimeout(function() {
+                            try{
+                                skipButton.click();
+                            }catch (error) {
+                                console.warn(error);
+                            }}, 800);
+                        try{
+                            skipButton.querySelector('span').innerHTML = "Skipping...";
+                        }catch (error) {
+                            console.warn(error);
+                        }
+                    }
+                }
+        });
+    });
+});
